f1179bf82f87af67944945838cf2cae7d2a325d0
poraspat98 <poraspat98@MWSRV-VS>
2019-03-30 13:04:00 +0000

    Start project; Create WordController with model and views;

---
+++ WordFrequency/Controllers/WordsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WordFrequency.Models;
+
+namespace WordFrequency.Controllers
+{
+    public class WordsController : Controller
+    {
+        private WordContext db = new WordContext();
+
+        // GET: Words
+        public ActionResult Index()
+        {
+            return View(db.Words.ToList());
+        }
+
+        // GET: Words/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Word word = db.Words.Find(id);
+            if (word == null)
+            {
+                return HttpNotFound();
+            }
+            return View(word);
+        }
+
+        // GET: Words/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Words/Create 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Content,Frequency,LearningRate")] Word word)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Words.Add(word);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(word);
+        }
+
+        // GET: Words/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Word word = db.Words.Find(id);
+            if (word == null)
+            {
+                return HttpNotFound();
+            }
+            return View(word);
+        }
+
+        // POST: Words/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Content,Frequency,LearningRate")] Word word)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(word).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(word);
+        }
+
+        // GET: Words/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Word word = db.Words.Find(id);
+            if (word == null)
+            {
+                return HttpNotFound();
+            }
+            return View(word);
+        }
+
+        // POST: Words/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Word word = db.Words.Find(id);
+            db.Words.Remove(word);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WordFrequency/Models/WordContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WordFrequency.Models
+{
+    public class WordContext : DbContext
+    {
+        public DbSet<Word> Words { get; set; }
+    }
+}
